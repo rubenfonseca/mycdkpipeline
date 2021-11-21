aa23e3a0000000109b30e5f571c344ee85114e4e
Ruben Fonseca <fonseka@gmail.com>
2021-11-21 20:26:36 +0100

    Initial commit

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,12 +1,12 @@
-import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
-import { CodePipeline, CodePipelineSource, ShellStep } from "@aws-cdk/pipelines";
-import { OtherApiStage } from './stages/other-api-stage';
-import { AmplifyStage } from './stages/amplify-stage';
+import {Construct, SecretValue, Stack, StackProps} from '@aws-cdk/core';
+import {CodePipeline, CodePipelineSource, ShellStep} from "@aws-cdk/pipelines";
+import {OtherApiStage} from './stages/other-api-stage';
+import {AmplifyStage} from './stages/amplify-stage';
 
 /**
  * The stack that defines the application pipeline
  */
- export class MyPipelineStack extends Stack {
+export class MyPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -14,18 +14,18 @@ import { AmplifyStage } from './stages/amplify-stage';
       // The pipeline name
       pipelineName: 'MyCdkPipeline',
 
-       // How it will be built and synthesized
-       synth: new ShellStep('Synth', {
-         // Where the source can be found
-         input: CodePipelineSource.gitHub('<YOUR_GITHUB_USERNAME>/mycdkpipeline', 'main'),
-         
-         // Install dependencies, build and run cdk synth
-         commands: [
-           'npm ci',
-           'npm run build',
-           'npx cdk synth'
-         ],
-       }),
+      // How it will be built and synthesized
+      synth: new ShellStep('Synth', {
+        // Where the source can be found
+        input: CodePipelineSource.gitHub('rubenfonseca/mycdkpipeline', 'main'),
+
+        // Install dependencies, build and run cdk synth
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth'
+        ],
+      }),
     });
 
     // This is where we add the application stages
